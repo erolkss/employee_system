e8c1710dc855a449054c788fff323d8dbbaed885
Lucas Lima <lucaserolima@gmail.com>
2024-01-26 20:31:51 -0400

    Adding page mappings to the 'HomeController' class

--- src/main/java/br/com/example/employee_system/controller/HomeController.java
+++ src/main/java/br/com/example/employee_system/controller/HomeController.java
@@ -5,5 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/loadEmpSave")
+    public String loadEmpSave() {
+        return "emp_save";
+    }
+    @GetMapping("/EditEmp")
+    public String editEmp() {
+        return "edit_emp";
+    }
 }
