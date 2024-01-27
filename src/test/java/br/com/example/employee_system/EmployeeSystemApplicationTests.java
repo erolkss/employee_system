20e0da25cf41fbf678a4802d4a03bd5e92bc64ac
Lucas Lima <lucaserolima@gmail.com>
2024-01-26 20:20:17 -0400

    First Commit

---
+++ src/test/java/br/com/example/employee_system/EmployeeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.employee_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
