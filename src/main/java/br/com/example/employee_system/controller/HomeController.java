92310972fd8aff753d9e4b52f3b9753cf1ed54f5
Lucas Lima <lucaserolima@gmail.com>
2024-01-27 19:41:16 -0400

    Created 'post' saveEmp() in 'HomeController'

--- src/main/java/br/com/example/employee_system/controller/HomeController.java
+++ src/main/java/br/com/example/employee_system/controller/HomeController.java
@@ -1,20 +1,42 @@
 package br.com.example.employee_system.controller;
 
+import br.com.example.employee_system.entity.Employee;
+import br.com.example.employee_system.service.EmployeeService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping("/")
     public String index() {
         return "index";
     }
+
     @GetMapping("/loadEmpSave")
     public String loadEmpSave() {
         return "emp_save";
     }
+
     @GetMapping("/EditEmp")
     public String editEmp() {
         return "edit_emp";
+    }
+
+    @PostMapping("/saveEmp")
+    public String saveEmp(@ModelAttribute Employee employee, HttpSession session) {
+        Employee newEmp = employeeService.saveEmp(employee);
+        if (newEmp != null) {
+            session.setAttribute("msg", "Register Successfully");
+        } else {
+            session.setAttribute("msg", "Something Wrong on Server");
+        }
+        return "redirect:/loadEmpSave";
     }
 }
