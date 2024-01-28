8ae5dc1919e43060af209baedcd60f4b254733b5
Lucas Lima <lucaserolima@gmail.com>
2024-01-27 20:03:23 -0400

    Created the method that will hide the Error and Success message when creating the Employee

--- src/main/java/br/com/example/employee_system/service/EmployeeServiceImpl.java
+++ src/main/java/br/com/example/employee_system/service/EmployeeServiceImpl.java
@@ -2,8 +2,11 @@ package br.com.example.employee_system.service;
 
 import br.com.example.employee_system.entity.Employee;
 import br.com.example.employee_system.repository.EmployeeRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.List;
 
@@ -35,5 +38,10 @@ public class EmployeeServiceImpl implements EmployeeService {
             return true;
         }
         return false;
+    }
+
+    public void removeSessionMessage(){
+        HttpSession session = ((ServletRequestAttributes)(RequestContextHolder.getRequestAttributes())).getRequest().getSession();
+        session.removeAttribute("msg");
     }
 }
